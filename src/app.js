a6a01a80f48c18e8d3c86341c780bab876854d54
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-06-09 06:50:27 +0800

    testing branch

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
-const Express       = require('express');
-const App           = Express();
-const Favicon       = require("serve-favicon");
-const BodyParser    = require('body-parser')
-const CookieParser  = require('cookie-parser');
-const Session       = require('express-session')
-const Constants     = require("./config/constants");
+const Express = require("express");
+const App = Express();
+const Favicon = require("serve-favicon");
+const BodyParser = require("body-parser");
+const CookieParser = require("cookie-parser");
+const Session = require("express-session");
+const Constants = require("./config/constants");
 
 /* 
     DOCU: Session Provider 
     Set the session settings to be used
+    testing
 */
 let session_setting = {
-    secret: Constants.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {}
-}
+	secret: Constants.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: true,
+	cookie: {},
+};
 
 /* 
     DOCU: Setup the express app settings 
@@ -26,17 +27,17 @@ App.use(CookieParser(Constants.SESSION_SECRET));
 App.set("view engine", "ejs");
 App.set("views", __dirname + "/views");
 App.use("/public", Express.static(__dirname + "/public"));
-App.use(BodyParser.json({limit: '50mb'}));
-App.use(BodyParser.urlencoded({limit: '50mb', extended: true}));
+App.use(BodyParser.json({ limit: "50mb" }));
+App.use(BodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 App.use(Favicon(__dirname + "/public/images/favicon.ico"));
 
 /* Routes */
-const UserViewRoutes       = require("./routes/view.user.routes");
+const UserViewRoutes = require("./routes/view.user.routes");
 
 /* Fair ROUTES */
 App.use("/", UserViewRoutes);
 
-App.listen(Constants.PORT, function(){
-    console.log('Your node js server is running on PORT ' + Constants.PORT);
+App.listen(Constants.PORT, function () {
+	console.log("Your node js server is running on PORT " + Constants.PORT);
 });
